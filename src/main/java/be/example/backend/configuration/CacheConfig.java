ac94a3473bf623079a104308b1e324536aa5940c
ragnor <ragnor84@gmail.com>
2014-11-25 20:07:01 +0100

    Fix cache configuration to work on jetty

--- src/main/java/be/example/backend/configuration/CacheConfig.java
+++ src/main/java/be/example/backend/configuration/CacheConfig.java
@@ -2,29 +2,36 @@ package be.example.backend.configuration;
 
 import com.google.code.ssm.Cache;
 import com.google.code.ssm.CacheFactory;
+import com.google.code.ssm.aop.CacheBase;
 import com.google.code.ssm.config.DefaultAddressProvider;
 import com.google.code.ssm.providers.spymemcached.MemcacheClientFactoryImpl;
 import com.google.code.ssm.providers.spymemcached.SpymemcachedConfiguration;
 import com.google.code.ssm.spring.SSMCache;
 import com.google.code.ssm.spring.SSMCacheManager;
+
 import net.spy.memcached.auth.AuthDescriptor;
 import net.spy.memcached.auth.PlainCallbackHandler;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.util.StringUtils;
 
 import javax.security.auth.callback.CallbackHandler;
+
 import java.util.HashSet;
 import java.util.Set;
 
 /**
  * Created by vanbosb on 22/11/14.
+ * This configuration set up only SSM artifacts/beans required by Spring Cache.
+ * To use SSM annotations more beans have to be created (all *CacheAdvice aspects).
  *
  * For more example code see:
  * http://docs.spring.io/spring/docs/current/javadoc-api/org/springframework/cache/annotation/EnableCaching.html
@@ -42,10 +49,15 @@ public class CacheConfig extends CachingConfigurerSupport {
     private Environment environment;
 
     @Bean
+    public CacheBase cacheBase() {
+        return new CacheBase();
+    }
+
+    @Bean
     @Override
     public CacheManager cacheManager() {
         Set<SSMCache> ssmCacheSet = new HashSet<>();
-        SSMCache ssmCache = new SSMCache(getDefaultCache(), 300, false);
+        SSMCache ssmCache = new SSMCache(defaultCache(), 300, false);
 
         ssmCacheSet.add(ssmCache);
 
@@ -54,7 +66,9 @@ public class CacheConfig extends CachingConfigurerSupport {
         return ssmCacheManager;
     }
 
-    public Cache getDefaultCache() {
+    @Bean
+    @DependsOn("cacheBase")
+    public CacheFactory cacheFactory() {
         CacheFactory cacheFactory = new CacheFactory();
         cacheFactory.setCacheName("defaultCache");
         cacheFactory.setCacheClientFactory(new MemcacheClientFactoryImpl());
@@ -77,7 +91,16 @@ public class CacheConfig extends CachingConfigurerSupport {
 
         cacheFactory.setConfiguration(cacheConfiguration);
 
-        return cacheFactory.getCache();
+        return cacheFactory;
+    }
+
+    @Bean
+    public Cache defaultCache() {
+        try {
+            return cacheFactory().getObject();
+        } catch (Exception e) {
+           throw new RuntimeException(e);
+        }
     }
 
 }
