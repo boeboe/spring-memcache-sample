e702ce43360513beff0130e6203cc8011675c393
Bart Van Bos <bartvanbos@gmail.com>
2014-11-26 19:27:53 +0100

    Fix issue in run.sh

--- run.sh
+++ run.sh
@@ -9,10 +9,8 @@ export PORT='9090'
 export BROWSER_URL='http://localhost:'${PORT}'/rest/persons'
 export BROWSER_URL_CACHE='http://localhost:'${PORT}'/rest/cache-persons'
 
-sleep 10 
-firefox ${BROWSER_URL} &> /dev/null &
-firefox ${BROWSER_URL_CACHE} &> /dev/null &
-
+sleep 10 && firefox ${BROWSER_URL} &> /dev/null &
+sleep 10 && firefox ${BROWSER_URL_CACHE} &> /dev/null &
 
 java -jar target/dependency/jetty-runner.jar --port ${PORT} target/*.war
 #java -jar target/dependency/webapp-runner.jar --port ${PORT} --expand-war target/*.war
